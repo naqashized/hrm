eaa2b4fb7be29342cf6ccbfc69cbd59d8b17a3ab
tmcshared <tahirnaqashized@gmail.com>
2018-07-19 16:24:52 +0500

    mongodb connection added

--- server/boot/sample-models.js
+++ server/boot/sample-models.js
@@ -9,71 +9,71 @@ module.exports = function(app) {
   var RoleMapping = app.models.RoleMapping;
   var Team = app.models.Team;
 
-  User.create([
-    {username: 'admin', email: 'admin@hrm.com', password: 'admin123'},
-    {username: 'test', email: 'test@hrm.com', password: 'test123'}
-  ], function(err, users) {
-    if (err) throw err;
+  // User.create([
+  //   {username: 'admin', email: 'admin@hrm.com', password: 'admin123'},
+  //   {username: 'test', email: 'test@hrm.com', password: 'test123'}
+  // ], function(err, users) {
+  //   if (err) throw err;
 
-    console.log('Created users:', users);
+  //   console.log('Created users:', users);
 
-    // create project 1 and make john the owner
-    users[0].projects.create({
-      name: 'project1',
-      balance: 100
-    }, function(err, project) {
-      if (err) throw err;
+  //   // create project 1 and make john the owner
+  //   users[0].projects.create({
+  //     name: 'project1',
+  //     balance: 100
+  //   }, function(err, project) {
+  //     if (err) throw err;
 
-      console.log('Created project:', project);
+  //     console.log('Created project:', project);
 
-      // add team members
-      Team.create([
-        {ownerId: project.ownerId, memberId: users[0].id},
-        {ownerId: project.ownerId, memberId: users[1].id}
-      ], function(err, team) {
-        if (err) throw err;
+  //     // add team members
+  //     Team.create([
+  //       {ownerId: project.ownerId, memberId: users[0].id},
+  //       {ownerId: project.ownerId, memberId: users[1].id}
+  //     ], function(err, team) {
+  //       if (err) throw err;
 
-        console.log('Created team:', team);
-      });
-    });
+  //       console.log('Created team:', team);
+  //     });
+  //   });
 
-    //create project 2 and make jane the owner
-    users[1].projects.create({
-      name: 'project2',
-      balance: 100
-    }, function(err, project) {
-      if (err) throw err;
+  //   //create project 2 and make jane the owner
+  //   users[1].projects.create({
+  //     name: 'project2',
+  //     balance: 100
+  //   }, function(err, project) {
+  //     if (err) throw err;
 
-      console.log('Created project:', project);
+  //     console.log('Created project:', project);
 
-      //add team members
-      Team.create({
-        ownerId: project.ownerId,
-        memberId: users[1].id
-      }, function(err, team) {
-        if (err) throw err;
+  //     //add team members
+  //     Team.create({
+  //       ownerId: project.ownerId,
+  //       memberId: users[1].id
+  //     }, function(err, team) {
+  //       if (err) throw err;
 
-        console.log('Created team:', team);
-      });
-    });
+  //       console.log('Created team:', team);
+  //     });
+  //   });
 
     //create the admin role
-    Role.create({
-      name: 'admin'
-    }, function(err, role) {
-      if (err) throw err;
+    // Role.create({
+    //   name: 'admin'
+    // }, function(err, role) {
+    //   if (err) throw err;
 
-      console.log('Created role:', role);
+    //   console.log('Created role:', role);
 
       //make bob an admin
-      role.principals.create({
-        principalType: RoleMapping.USER,
-        principalId: users[0].id
-      }, function(err, principal) {
-        if (err) throw err;
+  //     role.principals.create({
+  //       principalType: "Role",
+  //       principalId: '$authenticated'
+  //     }, function(err, principal) {
+  //       if (err) throw err;
 
-        console.log('Created principal:', principal);
-      });
-    });
-  });
+  //       console.log('Created principal:', principal);
+  //     });
+  //   });
+  // });
 };
